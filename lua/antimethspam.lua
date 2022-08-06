b7cd144cac3f44596c82e8bce910ac73eee50cfb
maromalo <54760464+maromalo@users.noreply.github.com>
2022-08-06 01:52:09 -0300

    Update anti spam to work with prefixes

--- lua/antimethspam.lua
+++ lua/antimethspam.lua
@@ -1,13 +1,12 @@
 local common_patterns = {
-    "add caustic soda",
-    "add muriatic acid",
-    "add hydrogen chloride",
-    "add cs", --just
-    "add mu", --in
-    "add hcl", --case
-    "ingredient added",
-    "batch complete",
-    "meth batch is done!"
+    "^add caustic soda$",
+    "^add muriatic acid$",
+    "^add hydrogen chloride$",
+    "^ingredient added$",
+    "^batch complete$",
+    "^meth batch is done!$",
+    "^%[Everything Meth%]",
+    "^%[Meth Helper%]"
 }
 
 --sooo i just learned it's "receive" and not "recieve"
@@ -16,7 +15,7 @@ local original_receive_message = ChatManager._receive_message
 function ChatManager:_receive_message(channel_id, name, message, ...)
     if (EverythingMeth.settings.anti_spam and (name ~= managers.network:session():local_peer():name() and name ~= "[" .. EverythingMeth:LocalizeLine("prefix") .. "]")) then
         for _, pattern in pairs(common_patterns) do
-            if (message:lower():match("^" .. pattern .. "$")) then
+            if (message:lower():match(pattern)) then
                 return false
             end
         end
